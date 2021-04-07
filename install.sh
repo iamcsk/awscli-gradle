b91790b73abf60c348dd43eaebf68afbfc9bc677
Chandrasekar <chandrasekaratsuresh@gmail.com>
2021-04-07 23:35:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,3 @@
-#update changes
-apt-get update
-
-# install deps
-apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-
-# install node js
-curl -sL https://deb.nodesource.com/setup_12.x | bash
-apt -y install nodejs
-
-#  semantic package
-npm install -g semantic-release @semantic-release/gitlab-config
-
-# install jdk
-apt-get install openjdk-13-jdk
-
-
-#install python
-apt-get install -y python3.6
-
-# install docker
-curl --tlsv1 https://get.docker.com/ | sh
-dockerd-rootless-setuptool.sh install
-
 # install aws cli
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
